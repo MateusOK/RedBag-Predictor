dba3f1467400128faa708865a5038d91debc9a3b
MateusOK <mateusribeiro-2004@hotmail.com>
2024-05-26 16:35:41 -0300

    feat: add cloudinary configuration

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import cloudinary
+from dotenv import load_dotenv
 import os
 from fastapi import FastAPI
-from dotenv import load_dotenv
 
 app = FastAPI()
 
 load_dotenv()
+
+cloud_name = os.getenv('CLOUD_NAME')
+api_key = os.getenv('API_KEY')
+api_secret = os.getenv('API_SECRET')
+
+cloudinary.config( 
+  cloud_name=cloud_name, 
+  api_key=api_key, 
+  api_secret=api_secret 
+)
 
 @app.get("/")
 async def root():
